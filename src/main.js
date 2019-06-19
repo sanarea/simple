8db0031346806b4f72c623e4bcacd16006a4034b
yangchigi <yangchigi@yangchigi.com>
2019-06-20 01:18:19 +0900

    ds

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import store from './store'
+import router from './router'
 
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify);
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
